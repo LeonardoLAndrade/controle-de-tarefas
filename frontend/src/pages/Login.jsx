b8df265adb93be4c07681c6901389341341189a2
LeonardoLAndrade <leonardolimaandrade40@gmail.com>
2024-08-23 16:52:53 -0400

    ✨ feat: Métodos de Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,20 +1,77 @@
 import Input from "../components/Input";
 import "./Login.css";
 import Label from "../components/Label";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [usuario, setUsuario] = useState({
+    codigo: "",
+    senha: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUsuario({ ...usuario, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (usuario.codigo && usuario.senha) {
+      fetch(`http://localhost:3003/sistema/usuarios/${usuario.codigo}`)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else if (response.status === 404) {
+            toast.error("Código de usuário não cadastrado.");
+          }
+          throw new Error("Erro ao buscar usuarios");
+        })
+        .then((data) => {
+          if (usuario.senha === data.senha) {
+            toast.success("Usuário logado com sucesso!");
+            setTimeout(() => navigate("/principal"), 1500);
+          } else {
+            toast.error("Senha incorreta.");
+          }
+        })
+        .catch((e) => {
+          toast.error("Erro ao fazer login.");
+        });
+    }
+  };
   return (
     <div className="mid">
       <h1 className="text-center">Controle de Tarefas</h1>
       <div className="box">
-        <form className="row g-2 form" action="/principal">
+        <form
+          className="row g-2 form"
+          action="/principal"
+          onSubmit={handleSubmit}
+        >
           <div className="col-md-3">
             <Label htmlFor="codUsuario" text="Usuário" />
-            <Input type="number" id="codUsuario" placeholder="Cod." />
+            <Input
+              type="number"
+              id="codUsuario"
+              name="codigo"
+              placeholder="Cod."
+              value={usuario.codigo}
+              onChange={handleChange}
+            />
           </div>
           <div className="col-md-6">
             <Label htmlFor="senha" text="Senha" />
-            <Input type="password" id="senha" placeholder="Insira a senha" />
+            <Input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="Insira a senha"
+              value={usuario.senha}
+              onChange={handleChange}
+            />
           </div>
           <div className="col-9">
             <button type="submit" className="btn btn-primary">

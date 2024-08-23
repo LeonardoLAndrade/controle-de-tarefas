1c94576863dd72e4cbd2f8be4f46881cdb71f5ef
LeonardoLAndrade <leonardolimaandrade40@gmail.com>
2024-08-23 16:46:09 -0400

    ✨ feat: Inclusão de rotas para os Controllers

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const UsuariosController = require("../controllers/usuariosController.js");
+const TarefasController = require("../controllers/tarefasController.js");
+const UsuarioTarefasController = require("../controllers/usuarioTarefasController.js");
 
+router.get("/usuarios/", UsuariosController.SearchAll);
+router.get("/usuarios/nextId", UsuariosController.GetNextUserId);
+router.get("/usuarios/:id", UsuariosController.SearchOne);
 router.post("/cadastro", UsuariosController.Insert);
+
+router.get("/tarefas/", TarefasController.SearchAll);
+router.get("/tarefas/nextId", TarefasController.GetNextTaskId);
+router.get("/tarefas/:id", TarefasController.SearchOne);
+router.post("/novaTarefa", TarefasController.Insert);
+
+router.get("/TarefasDoUsuario", UsuarioTarefasController.SearchAllTarefas);
+router.post("/novaTarefaUsuario", UsuarioTarefasController.Insert);
 
 module.exports = router;
